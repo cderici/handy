a451e50faea24e4402e952e365082305187bf2ea
Caner Derici <canerderici@gmail.com>
2016-11-05 22:39:09 -0400

    can lift lambdas with free vars inside

--- lambda-lift.rkt
+++ lambda-lift.rkt
@@ -6,6 +6,8 @@
                      syntax/parse
                      syntax/free-vars))
 
+(require "unroll.rkt")
+
 ;; TODO
 ;; 1 - lift a single lambda with a free variable (figure out a way to modify the call sites)
 ;;     (map (lambda (x) k) '(1 2 3))
@@ -16,14 +18,16 @@
     #:literals (define lambda define/unroll unroll)
     [(_ (~and lam (lambda (arg:id ...) body ...)))
      (define exp (local-expand #'lam 'expression '()))
+     (define frees (free-vars exp))
+     
      (syntax-parse exp
        #:literals (#%plain-lambda)
        [(#%plain-lambda (larg ...) lbody)
-        (define frees (free-vars #'lbody))
-        (when (not (= (length frees) (length (syntax-e #'(arg ...)))))
-          (error 'lift-this "cannot lift a lambda expression with a free variable"))
-        (with-syntax ([lam-id (syntax-local-lift-expression #'lam)])
-          #'lam-id)])]
+        (with-syntax* ([(free-ids ...) frees]
+                       [lifted-id (syntax-local-lift-expression
+                                   #'(lambda (free-ids ...)
+                                       (lambda (larg ...) lbody)))])
+          #'(lifted-id free-ids ...))])]
     [(_ (~and def (define (f:id arg:id ...) body ...)))
      ;; expanding for free-vars (-it only accepts core forms-)
      (define exp (local-expand #'(letrec ([f (lambda (arg ...) body ...)]) f) 'expression '()))
@@ -50,14 +54,16 @@
        #'(lift-this (define (f arg ...) (unroll n f (arg ...) body body )...))]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-#|
+
 (define (top-func n) (* n n))
 
 (define (func)
-  (define k 1)
+  (define k 2)
   ;; lambda
-  ;((lift-this (lambda (x) (+ (top-func x) x))) k)
-  ;((lift-this (lambda () (top-func k))) 3)
+  ((lift-this (lambda (x) (+ (top-func x) x))) k)
+  ((lift-this (lambda (x) (top-func k))) 3)
+
+  (map (lift-this (lambda (x) (+ x k))) '(1 2 3))
   
   ;(lift-this (define (bar x) 1 k))
   ;(bar 2)
@@ -71,10 +77,10 @@
   ;; lift + unroll
   #;(lift-this (define/unroll 2 (fact n)
                (if (<= n 1) 1 (* n (fact (- n 1))))))
-  (lift-this (define/unroll 2 (fact n) n))
-  (fact 4)
+  #;(lift-this (define/unroll 2 (fact n) n))
+  #;(fact 4)
 )
 
 (func)
-|#
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
